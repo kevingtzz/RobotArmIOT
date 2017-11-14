bb0711589914bec6a300e60e3a96d14b8da703be
Kevin Gutierrez <kgutierreg@eafit.edu.co>
2017-11-14 12:45:59 -0500

    Local driving done.

--- RobotArmIOT.ino
+++ RobotArmIOT.ino
@@ -5,18 +5,12 @@
 
 #include <Servo.h>
 
-/*#define spinS 5
-#define arm1S 4
-#define arm2S 3
-#define arm3S 2
-#define gripS 1
-*/
 //Potentiometer pin declaration.
 int spinP = 0;  
 int arm1P = 1;
 int arm2P = 2;
 int arm3P = 3;
-int gripP = 4;
+int gripP = 5;
  
 //Potentiometer var declaration.
 int spinV;  
@@ -36,19 +30,7 @@ void setup() {
   arm2.attach(3);  //D2
   arm3.attach(2);   //D3
   grip.attach(1);   //D4
-/*
-  delay(100);
-  spin.write(90);
-  delay(100);
-  arm1.write(90);
-  delay(100);
-  arm2.write(90);
-  delay(100);
-  arm3.write(90);
-  delay(100);
-  grip.write(70);
-  delay(100);
-*/
+
   spinV = analogRead(spinP);
   delay(1);
   arm1V = analogRead(arm1P);
@@ -60,7 +42,9 @@ void setup() {
   gripV = analogRead(gripP);
   delay(1);
 
-  Serial.begin(9600);
+  grip.write(25);
+
+  Serial.begin(2400);
 }
 
 void loop() {
@@ -70,29 +54,29 @@ void loop() {
       delay(1);
       spinV = map(spinV,0,1023,0,180);
       spin.write(spinV);
-      Serial.println("spin: " + spinV);
+      //Serial.println("spin: " + spinV);
      
       arm1V = analogRead(arm1P);
       delay(1);
       arm1V = map(arm1V,0,1023,0,180);
       arm1.write(arm1V);
-      Serial.println("arm1: " + arm1V);
+      //Serial.println("arm1: " + arm1V);
 
       arm2V = analogRead(arm2P);
       delay(1);
       arm2V = map(arm2V,0,1023,0,180);
       arm2.write(arm2V);
-      Serial.println("arm2: " + arm2V);
+      //Serial.println("arm2: " + arm2V);
 
       arm3V = analogRead(arm3P);
       delay(1);
       arm3V = map(arm3V,0,1023,0,180);
       arm3.write(arm3V);
-      Serial.println("arm3: " + arm3V);
+      //Serial.println("arm3: " + arm3V);
 
       gripV = analogRead(gripP);
       delay(1);
       gripV = map(gripV,0,1023,0,180);
       grip.write(gripV);
-      Serial.println("grip: " + gripV);
+      Serial.println(gripV);
 }
